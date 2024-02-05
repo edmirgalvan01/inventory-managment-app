f9d5a8286a879e5f90b998dace558ab7fb6351bc
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-05 10:26:53 -0600

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "soft-white": "#FBF6EE",
+        "soft-black": "#191919",
+        "error-red": "#BF3131",
+        "success-green": "#79AC78",
+        "hard-blue": "#29353C",
+        "medium-blue": "#44576D",
+        blue: "#768A96",
+        "light-blue": "#AAC7D8",
+        "soft-blue": "#DFEBF6",
+      },
+    },
   },
   plugins: [],
 };
