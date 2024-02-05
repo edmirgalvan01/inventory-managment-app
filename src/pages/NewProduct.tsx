dbaf89998070cd2a7d10e5b03e4292f92f215d89
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-05 13:37:29 -0600

    Create NewProduct page

--- src/pages/NewProduct.tsx
+++ src/pages/NewProduct.tsx
@@ -1,24 +1,47 @@
+import { useNavigate } from "react-router-dom";
 import { PrimaryButton } from "../components/Buttons";
 import { FieldInput, FieldSelect } from "../components/FieldInput";
 import PageTitle from "../components/PageTitle";
+import { useInsertProduct } from "../hooks/useInsertProduct";
 
 export default function NewProductPage() {
+  const navigate = useNavigate();
+  const {
+    product,
+    updateProduct: handleChange,
+    saveProduct,
+  } = useInsertProduct();
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    saveProduct(product).then((response) => {
+      if (!response.error) navigate("/inventory");
+    });
+  };
+
   return (
     <section className="containerPage">
       <PageTitle title="Nuevo producto" />
-      <form className="flex flex-col gap-[20px]">
+      <form className="flex flex-col gap-[20px]" onSubmit={handleSubmit}>
         <FieldInput
           label="Nombre"
           placeholder="Color azul"
           disabled={false}
           type="text"
-          handleClick={() => {}}
+          handleChange={(e) => handleChange("name", e.target.value)}
+          required
         />
-        <FieldSelect label="Cantidad" disabled={false} handleClick={() => {}} />
+        <FieldSelect
+          label="Unidad"
+          disabled={false}
+          handleChange={(e) => handleChange("unity", e.target.value)}
+          required
+        />
         <FieldInput
           label="Cantidad"
           disabled={false}
-          handleClick={() => {}}
+          handleChange={(e) => handleChange("quantity", e.target.value)}
+          required
           placeholder="12"
           type="number"
         />
@@ -27,7 +50,8 @@ export default function NewProductPage() {
           placeholder="43.23"
           disabled={false}
           type="number"
-          handleClick={() => {}}
+          handleChange={(e) => handleChange("price", e.target.value)}
+          required
         />
         <PrimaryButton>Guardar producto</PrimaryButton>
       </form>
