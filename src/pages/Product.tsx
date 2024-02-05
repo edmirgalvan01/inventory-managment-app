bd8f0c479226b0160dd0039b062fe4faf8e5a0ec
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-05 13:10:31 -0600

    Add dynamic content to Product page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,24 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { PrimaryButton, SecondaryButton } from "../components/Buttons";
 import ItemData from "../components/ItemData";
 import PageTitle from "../components/PageTitle";
+import { useGetProductById } from "../hooks/useGetProductById";
 
 export default function ProductPage() {
+  const { productId } = useParams();
+  const fixedId = Number(productId);
+
+  const { product, error, isLoading } = useGetProductById(fixedId);
+
   return (
     <section className="containerPage">
-      <PageTitle title="Nombre del producto" />
-      <ItemData title="Nombre" text="Color Azul" />
-      <ItemData title="Cantidad" text="22 unidades" />
-      <ItemData title="Precio" text="$34.78" />
-      <ItemData title="Unidad" text="Lts" />
-      <ItemData title="Ventas" text="54 ventas" />
+      {isLoading ? (
+        <p>Cargando...</p>
+      ) : product ? (
+        <>
+          <PageTitle title="Nombre del producto" />
+          <ItemData title="Nombre" text={product.name} />
+          <ItemData
+            title="Cantidad"
+            text={`${product.quantity} ${product.unity}`}
+          />
+          <ItemData title="Precio" text={`$${product.price}`} />
+          <ItemData title="Unidad" text={product.unity} />
 
-      <div className="flex flex-col gap-[10px]">
-        <PrimaryButton>
-          <Link to="/edit-product/12">Editar</Link>
-        </PrimaryButton>
-        <SecondaryButton>Borrar</SecondaryButton>
-      </div>
+          <div className="flex flex-col gap-[10px]">
+            <PrimaryButton>
+              <Link to="/edit-product/12">Editar</Link>
+            </PrimaryButton>
+            <SecondaryButton>Borrar</SecondaryButton>
+          </div>
+        </>
+      ) : (
+        <p>{error?.message}</p>
+      )}
     </section>
   );
 }
