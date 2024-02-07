d9750686cf532765ab5522c9ea42cfa1c8881f4d
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-07 12:58:09 -0600

    Change options

--- src/pages/NewSale.tsx
+++ src/pages/NewSale.tsx
@@ -1,36 +1,52 @@
 import { PrimaryButton } from "../components/Buttons";
 import { FieldInput, FieldSelect } from "../components/FieldInput";
+import { useGetProducts } from "../hooks/useGetProducts";
 import PageTitle from "../components/PageTitle";
 
 export default function NewSalePage() {
+  const { products, isLoading } = useGetProducts();
+  const mappedOptions = products?.map((product) => {
+    return {
+      value: product.id,
+      label: product.name,
+    };
+  });
+
   return (
     <section className="containerPage">
       <PageTitle title="Nueva venta" />
-      <form className="flex flex-col gap-[20px]">
-        <FieldSelect
-          label="Producto"
-          disabled={false}
-          handleChange={() => {}}
-          required
-        />
-        <FieldInput
-          label="Cantidad"
-          placeholder="14"
-          disabled={false}
-          type="text"
-          handleChange={() => {}}
-          required
-        />
-        <FieldInput
-          label="Fecha"
-          disabled={false}
-          handleChange={() => {}}
-          required
-          placeholder="12/02/2024"
-          type="date"
-        />
-        <PrimaryButton>Guardar venta</PrimaryButton>
-      </form>
+      <>
+        {isLoading ? (
+          <p>Cargando...</p>
+        ) : (
+          <form className="flex flex-col gap-[20px]">
+            <FieldSelect
+              options={mappedOptions}
+              label="Producto"
+              disabled={false}
+              handleChange={() => {}}
+              required
+            />
+            <FieldInput
+              label="Cantidad"
+              placeholder="14"
+              disabled={false}
+              type="text"
+              handleChange={() => {}}
+              required
+            />
+            <FieldInput
+              label="Fecha"
+              disabled={false}
+              handleChange={() => {}}
+              required
+              placeholder="12/02/2024"
+              type="date"
+            />
+            <PrimaryButton>Guardar venta</PrimaryButton>
+          </form>
+        )}
+      </>
     </section>
   );
 }
