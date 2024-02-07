ccca89b1b8bf6442b2044ce01088a85e6da5babe
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-07 13:52:41 -0600

    Use useInsertSale

--- src/pages/NewSale.tsx
+++ src/pages/NewSale.tsx
@@ -2,15 +2,28 @@ import { PrimaryButton } from "../components/Buttons";
 import { FieldInput, FieldSelect } from "../components/FieldInput";
 import { useGetProducts } from "../hooks/useGetProducts";
 import PageTitle from "../components/PageTitle";
+import { useInsertSale } from "../hooks/useInsertSale";
 
 export default function NewSalePage() {
   const { products, isLoading } = useGetProducts();
+  const { updateSale: handleChange, saveSale } = useInsertSale();
+
   const mappedOptions = products?.map((product) => {
     return {
       value: product.id.toString(),
       label: product.name,
     };
   });
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    saveSale()
+      .then(({ data, error }) => {
+        // Si no hay error, mostrar una notificacion de ✅
+        // y ridirigir a Sales
+      })
+      .finally(() => {});
+  };
 
   return (
     <section className="containerPage">
@@ -19,12 +32,12 @@ export default function NewSalePage() {
         {isLoading ? (
           <p>Cargando...</p>
         ) : (
-          <form className="flex flex-col gap-[20px]">
+          <form className="flex flex-col gap-[20px]" onSubmit={handleSubmit}>
             <FieldSelect
               options={mappedOptions}
               label="Producto"
               disabled={false}
-              handleChange={() => {}}
+              onChange={(e) => handleChange("product_id", e.target.value)}
               required
             />
             <FieldInput
@@ -32,16 +45,24 @@ export default function NewSalePage() {
               placeholder="14"
               disabled={false}
               type="text"
-              handleChange={() => {}}
+              onChange={(e) => handleChange("quantity", e.target.value)}
               required
             />
             <FieldInput
               label="Fecha"
               disabled={false}
-              handleChange={() => {}}
+              onChange={(e) => handleChange("date", e.target.value)}
               required
               placeholder="12/02/2024"
               type="date"
+            />
+            <FieldInput
+              label="Total"
+              disabled={false}
+              onChange={(e) => handleChange("total", e.target.value)}
+              required
+              placeholder="$345.32"
+              type="number"
             />
             <PrimaryButton>Guardar venta</PrimaryButton>
           </form>
