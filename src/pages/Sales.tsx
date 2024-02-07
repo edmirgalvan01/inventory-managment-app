459fd4982cbb42c7ac9773d22371cc584ad473fd
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-07 12:30:03 -0600

    Use hook useGetSales and handle errors

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -2,15 +2,24 @@ import { Link } from "react-router-dom";
 import { PrimaryButton } from "../components/Buttons";
 import ListOfItemsWithTitle from "../components/ListOfItemsWithTitle";
 import PageTitle from "../components/PageTitle";
+import { useGetSales } from "../hooks/useGetSales";
 
 export default function SalesPage() {
+  const { sales, error, isLoading } = useGetSales();
+
   return (
     <section className="containerPage">
       <PageTitle title="Ventas" />
       <PrimaryButton>
         <Link to="/new-sale">Nueva venta</Link>
       </PrimaryButton>
-      <ListOfItemsWithTitle title="Ventas (16)" type="sale" list={[]} />
+      {!isLoading ? (
+        <ListOfItemsWithTitle title="Ventas (16)" type="sale" list={sales!} />
+      ) : error ? (
+        <p>{error.message}</p>
+      ) : (
+        <p>Cargando...</p>
+      )}
     </section>
   );
 }
