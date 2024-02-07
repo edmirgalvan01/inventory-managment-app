56b47ab9163605485d3bd18f4379e9e8d540118f
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-07 16:40:03 -0600

    Use useUpdateProduct hook

--- src/pages/EditProduct.tsx
+++ src/pages/EditProduct.tsx
@@ -2,49 +2,63 @@ import { useParams } from "react-router-dom";
 import { PrimaryButton } from "../components/Buttons";
 import { FieldInput, FieldSelect } from "../components/FieldInput";
 import PageTitle from "../components/PageTitle";
-import { useGetProductById } from "../hooks/useGetProductById";
 import { LIST_OF_UNITIES } from "../consts";
+import { useUpdateProduct } from "../hooks/useUpdateProduct";
 
 export default function EditProductPage() {
   const { productId } = useParams();
   const fixedId = Number(productId);
 
-  const { product } = useGetProductById(fixedId);
+  const {
+    newProduct,
+    updateNewProduct: handleChange,
+    saveNewProduct,
+  } = useUpdateProduct(fixedId);
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    saveNewProduct();
+  };
 
   return (
     <section className="containerPage">
       <PageTitle title="Editar producto" />
-      <form className="flex flex-col gap-[20px]">
+      <form className="flex flex-col gap-[20px]" onSubmit={handleSubmit}>
         <FieldInput
           label="Nombre"
-          placeholder={`${product?.name}`}
+          placeholder={`${newProduct?.newName}`}
           disabled={false}
-          onChange={() => {}}
+          onChange={(e) => handleChange("newName", e.target.value)}
           required
           type="text"
+          value={newProduct.newName}
         />
         <FieldSelect
           label="Unidad"
           options={LIST_OF_UNITIES}
           disabled={false}
-          onChange={() => {}}
+          onChange={(e) => handleChange("newUnity", e.target.value)}
           required
+          value={newProduct.newUnity}
         />
         <FieldInput
           label="Cantidad"
-          placeholder={`${product?.quantity}`}
+          placeholder={`${newProduct?.newQuantity}`}
           disabled={false}
-          onChange={() => {}}
+          onChange={(e) => handleChange("newQuantity", e.target.value)}
           required
           type="number"
+          value={newProduct.newQuantity}
         />
         <FieldInput
           label="Precio unitario"
-          placeholder={`$${product?.price}`}
+          placeholder={`$${newProduct?.newPrice}`}
           disabled={false}
-          onChange={() => {}}
+          onChange={(e) => handleChange("newPrice", e.target.value)}
           required
           type="number"
+          value={newProduct.newPrice}
+          step="0.01"
         />
         <PrimaryButton>Guardar producto</PrimaryButton>
       </form>
