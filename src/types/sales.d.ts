1e86d22aa8d7adb2368d9fe304bca9ea2514f5c8
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-07 14:24:32 -0600

    Create SaleTypeWithProduct

--- src/types/sales.d.ts
+++ src/types/sales.d.ts
@@ -1,9 +1,20 @@
+import { ProductType } from "./products";
+
 export type SaleType = {
   id: number;
   quantity: float;
   total: float;
   product_id: number;
   date: string;
+};
+
+export type SaleTypeWithProduct = {
+  id: number;
+  quantity: float;
+  total: float;
+  product_id: number;
+  date: string;
+  products: ProductType;
 };
 
 export type SaleWithoutIdType = {
