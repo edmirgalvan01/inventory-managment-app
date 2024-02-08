7bbfb0e03bcece9a120f60cc7790e199fdae2015
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-08 06:44:26 -0600

    Change newProduct properties

--- src/hooks/useUpdateProduct.ts
+++ src/hooks/useUpdateProduct.ts
@@ -1,22 +1,26 @@
 import { useEffect, useState } from "react";
 import { useGetProductById } from "./useGetProductById";
+import { updateProduct } from "../database/products.service";
+import { ProductWithoutIdType } from "../types/products";
 
 export function useUpdateProduct(productId: number) {
   const { product } = useGetProductById(productId);
 
-  const [newProduct, setNewProduct] = useState({
-    newName: product?.name,
-    newUnity: product?.unity,
-    newQuantity: product?.quantity,
-    newPrice: product?.price,
+  const [newProduct, setNewProduct] = useState<
+    ProductWithoutIdType | undefined
+  >({
+    name: product?.name ?? "",
+    unity: product?.unity ?? "",
+    quantity: product?.quantity ?? "",
+    price: product?.price ?? "",
   });
 
   useEffect(() => {
     setNewProduct({
-      newName: product?.name,
-      newUnity: product?.unity,
-      newQuantity: product?.quantity,
-      newPrice: product?.price,
+      name: product?.name ?? "",
+      unity: product?.unity ?? "",
+      quantity: product?.quantity ?? "",
+      price: product?.price ?? "",
     });
   }, [product]);
 
@@ -24,18 +28,18 @@ export function useUpdateProduct(productId: number) {
     setNewProduct({
       ...newProduct,
       [property]: value,
-    });
+    } as ProductWithoutIdType);
   };
 
   const saveNewProduct = () => {
     const productToSave = {
-      name: newProduct.newName,
-      unity: newProduct.newUnity,
-      quantity: newProduct.newQuantity,
-      price: newProduct.newPrice,
+      name: newProduct!.name,
+      unity: newProduct!.unity,
+      quantity: newProduct!.quantity,
+      price: newProduct!.price,
     };
 
-    //TODO: saveNewProduct(newProduct)
+    updateProduct(productId, productToSave);
   };
 
   return { newProduct, updateNewProduct, saveNewProduct };
