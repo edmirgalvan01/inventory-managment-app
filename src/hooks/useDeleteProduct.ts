85e9992702081a010f898d1a08c8be445555a968
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-09 06:50:42 -0600

    Add error handler to useDeleteProduct

--- src/hooks/useDeleteProduct.ts
+++ src/hooks/useDeleteProduct.ts
@@ -1,15 +1,26 @@
 import { useState } from "react";
 import { deleteProduct } from "../database/products.service";
 import { PostgrestError } from "@supabase/supabase-js";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
 
 export function useDeleteProduct() {
   const [error, setError] = useState<PostgrestError | null>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  const navigate = useNavigate();
+
   const removeProduct = (productId: number) => {
     deleteProduct(productId)
       .then(({ error }) => {
-        if (error) setError(error);
+        if (error) {
+          setError(error);
+        } else {
+          toast.success("Producto eliminado correctamente.");
+          setTimeout(() => {
+            navigate("/sales");
+          }, 1500);
+        }
       })
       .finally(() => {
         setIsLoading(false);
