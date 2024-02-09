2dad8c5c38705f5caa123361fb5170650e51045b
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-09 06:43:20 -0600

    Add all properties to button components

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,18 +1,24 @@
-interface Props {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function PrimaryButton({ children }: Props) {
+export function PrimaryButton({ children, ...rest }: Props) {
   return (
-    <button className="font-inter h-10 px-4 text-sm rounded-md font-semibold bg-medium-blue text-soft-white flex justify-center items-center">
+    <button
+      {...rest}
+      className="font-inter h-10 px-4 text-sm rounded-md font-semibold bg-medium-blue text-soft-white flex justify-center items-center"
+    >
       {children}
     </button>
   );
 }
 
-export function SecondaryButton({ children }: Props) {
+export function SecondaryButton({ children, ...rest }: Props) {
   return (
-    <button className="font-inter h-10 px-4 text-sm rounded-md font-semibold bg-transparent text-medium-blue flex justify-center items-center border-[2px] border-medium-blue">
+    <button
+      {...rest}
+      className="font-inter h-10 px-4 text-sm rounded-md font-semibold bg-transparent text-medium-blue flex justify-center items-center border-[2px] border-medium-blue"
+    >
       {children}
     </button>
   );
