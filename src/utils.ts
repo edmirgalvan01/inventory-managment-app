4fdc6479f64f704488f7f99267161d91c490fbc4
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-10 10:23:20 -0600

    Create function to calculate new stock

--- src/utils.ts
+++ src/utils.ts
@@ -24,3 +24,26 @@ export async function checkIfProductIsAvailable(
     return { success: true, error: "" };
   }
 }
+
+export async function calculateNewStock(
+  productId: number,
+  quantitySold: number
+) {
+  // 1. Get the product sold by ID
+  const productResponse = await getProductById(productId);
+
+  // 2. If there was an error or the product does not exist, return an error.
+  if (productResponse.error || productResponse.products?.length === 0) {
+    console.error("calculateNewStock", productResponse.error);
+    return {
+      success: false,
+      newStock: 0,
+      error: "The product is not available",
+    };
+  }
+
+  const product = productResponse.products![0];
+  const newStock = product.quantity - quantitySold;
+
+  return { success: true, error: "", newStock };
+}
