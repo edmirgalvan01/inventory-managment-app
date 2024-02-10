9ef4eb5e0aaaa7f7029ab107c3544b9b8e943a55
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-10 09:52:57 -0600

    Change comments to english

--- src/utils.ts
+++ src/utils.ts
@@ -4,23 +4,23 @@ export async function checkIfProductIsAvailable(
   productId: number,
   quantitySold: number
 ) {
-  // 1. Obtener el producto vendido por su ID
+  // 1. Get the product sold by ID
   const productResponse = await getProductById(productId);
 
-  // 2. Si hubo un error o el producto no existe, retornar un mensaje de error
+  // 2. If there was an error or the product does not exist, return an error.
   if (productResponse.error || productResponse.products?.length === 0) {
     console.error("checkIfProductIsAvailable", productResponse.error);
     return { success: false, error: "The product is not available" };
   }
 
-  // 3. Si el producto existe, verificar que la cantidad vendida sea menor o igual al stock del producto
   const product = productResponse.products![0];
 
+  // 3. If there's a product, verify that the quantity sold is less than or equal to the stock of the product.
   if (quantitySold > product.quantity) {
-    // 4. Si la cantidad es mayor al stock, retornar un mensaje de error
+    // 4. If quantity is greater than the stock, return an error.
     return { success: false, error: "Not enough product in stock" };
   } else {
-    // 5. Si la cantidad es menor o igual al stock, retornar true
+    // 5. If quantity is less than or equal to stock, return true
     return { success: true, error: "" };
   }
 }
