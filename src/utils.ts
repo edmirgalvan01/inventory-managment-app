6670ce581f303f9e28ad0d10981e9a826130b67d
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-10 11:12:04 -0600

    Change error messages

--- src/utils.ts
+++ src/utils.ts
@@ -10,7 +10,7 @@ export async function checkIfProductIsAvailable(
   // 2. If there was an error or the product does not exist, return an error.
   if (productResponse.error || productResponse.products?.length === 0) {
     console.error("checkIfProductIsAvailable", productResponse.error);
-    return { success: false, error: "The product is not available" };
+    return { success: false, error: "El producto no esta disponible" };
   }
 
   const product = productResponse.products![0];
@@ -18,7 +18,7 @@ export async function checkIfProductIsAvailable(
   // 3. If there's a product, verify that the quantity sold is less than or equal to the stock of the product.
   if (quantitySold > product.quantity) {
     // 4. If quantity is greater than the stock, return an error.
-    return { success: false, error: "Not enough product in stock" };
+    return { success: false, error: "No hay suficiente producto en stock" };
   } else {
     // 5. If quantity is less than or equal to stock, return true
     return { success: true, error: "" };
