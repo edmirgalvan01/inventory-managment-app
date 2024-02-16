285b3f320c4ed2cea4bea7735bb39a90473e957d
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-16 17:16:17 -0600

    Use GroupSalesByMonth

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -1,26 +1,16 @@
 import { Link } from "react-router-dom";
 import { PrimaryButton } from "../components/Buttons";
-import ListOfItemsWithTitle from "../components/ListOfItemsWithTitle";
 import PageTitle from "../components/PageTitle";
-import { useGetSales } from "../hooks/useGetSales";
+import { GroupByMonth } from "../components/GroupByMonth";
 
 export default function SalesPage() {
-  const { sales, error, isLoading } = useGetSales();
-  const totalSales = sales?.length;
-
   return (
     <section className="containerPage">
       <PageTitle title="Ventas" />
       <PrimaryButton>
         <Link to="/new-sale">Nueva venta</Link>
       </PrimaryButton>
-      <ListOfItemsWithTitle
-        isLoading={isLoading}
-        title={`Ventas (${totalSales})`}
-        type="sale"
-        list={sales!}
-        error={error}
-      />
+      <GroupByMonth monthNumber={1} />
     </section>
   );
 }
