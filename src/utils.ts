ff5d7df0761ed0c327f6e428ba897277819becdb
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-16 17:44:10 -0600

    Delete some comments

--- src/utils.ts
+++ src/utils.ts
@@ -4,10 +4,8 @@ export async function checkIfProductIsAvailable(
   productId: number,
   quantitySold: number
 ) {
-  // 1. Get the product sold by ID
   const productResponse = await getProductById(productId);
 
-  // 2. If there was an error or the product does not exist, return an error.
   if (productResponse.error || productResponse.products?.length === 0) {
     console.error("checkIfProductIsAvailable", productResponse.error);
     return { success: false, error: "El producto no esta disponible" };
@@ -15,12 +13,9 @@ export async function checkIfProductIsAvailable(
 
   const product = productResponse.products![0];
 
-  // 3. If there's a product, verify that the quantity sold is less than or equal to the stock of the product.
   if (quantitySold > product.quantity) {
-    // 4. If quantity is greater than the stock, return an error.
     return { success: false, error: "No hay suficiente producto en stock" };
   } else {
-    // 5. If quantity is less than or equal to stock, return true
     return { success: true, error: "" };
   }
 }
