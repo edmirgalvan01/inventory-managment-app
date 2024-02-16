fd4a00f3b2eab37ccaece485d7e2c97e14f126de
Edmir Galvan <edmirgalvaz@gmail.com>
2024-02-16 17:25:26 -0600

    Change response data

--- src/hooks/useGetSalesByMonth.ts
+++ src/hooks/useGetSalesByMonth.ts
@@ -5,7 +5,7 @@ import { getSalesByMonth } from "../database/sales.service";
 import { MONTHS_RANGE } from "../consts";
 
 export function useGetSalesByMonth(monthNumber: keyof typeof MONTHS_RANGE) {
-  const [sales, setSales] = useState<SaleType[] | null>([]);
+  const [sales, setSales] = useState<SaleType[]>([]);
   const [error, setError] = useState<PostgrestError | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
@@ -13,7 +13,7 @@ export function useGetSalesByMonth(monthNumber: keyof typeof MONTHS_RANGE) {
     try {
       const response = await getSalesByMonth(monthNumber);
       setError(response.error);
-      setSales(response.data);
+      setSales(response.data ?? []);
     } finally {
       setIsLoading(false);
     }
